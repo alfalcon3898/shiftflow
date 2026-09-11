e15ad1e923c4eb8c8d0d5c0fc9877190b80117a2
alfalcon3898 <maryann.falcon@snhu.edu>
2026-09-11 04:04:21 -0500

    Added test for get_role

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -3,4 +3,8 @@ from employee import Employee
 def test_creates_employee_with_valid_data():
     emp = Employee("Allen" , "SL")
     assert emp.get_name() == "Allen"
+
+def test_get_role_returns_correct_role():
+    emp = Employee("Allen", "SL")
     assert emp.get_role() == "SL"
+    
