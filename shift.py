f33a9bf0ff49198fe6e57e7e243933810c0cf171
Allen Falcon <allenfalcon3898@gmail.com>
2026-09-16 08:49:35 -0500

    Add getters to Shift class

--- shift.py
+++ shift.py
@@ -5,3 +5,15 @@ class Shift:
         self.__date = date
         self.__start_time = start_time
         self.__end_time = end_time
+
+    #--- Getters---
+    def get_employee(self)->Employee:
+        return self.__employee
+    def get_date(self)->str:
+        return self.__date
+    def get_start_time(self)->str:
+        return self.__start_time
+    def get_end_time(self)->str:
+        return self.__end_time
+    
+
