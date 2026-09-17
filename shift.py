7af5cb4a0ca4eda14d1bb792e04907b4e2090205
Allen Falcon <allenfalcon3898@gmail.com>
2026-09-17 01:43:59 -0500

    Add Shift input validation

--- shift.py
+++ shift.py
@@ -2,9 +2,14 @@ from employee import Employee
 from datetime import datetime, timedelta
 class Shift:
     def __init__(self, employee:Employee, date:str, start_time:str, end_time:str ) -> None:
+        if not isinstance(employee, Employee):
+            raise TypeError("Employee must be an Employee object.")
         self.__employee = employee
+        datetime.strptime(date, "%Y-%m-%d")
         self.__date = date
+        datetime.strptime(start_time, "%I:%M %p")
         self.__start_time = start_time
+        datetime.strptime(end_time, "%I:%M %p")
         self.__end_time = end_time
 
     #--- Getters---
@@ -19,10 +24,13 @@ class Shift:
 
     #---Setters---
     def set_date(self,date:str)-> None:
+        datetime.strptime(date, "%Y-%m-%d")
         self.__date = date
     def set_start_time(self, start_time:str)-> None:
+        datetime.strptime(start_time, "%I:%M %p")
         self.__start_time = start_time
     def set_end_time(self, end_time:str)-> None:
+        datetime.strptime(end_time, "%I:%M %p")
         self.__end_time = end_time
 
     def calculate_shift_hr(self) -> float:
