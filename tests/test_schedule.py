ef12cadd33d4aa1660041bef08d29121f851ca4b
alfalcon3898 <maryann.falcon@snhu.edu>
2026-09-18 03:04:02 -0500

    Add Schedule shift storage and initial tests

---
+++ tests/test_schedule.py
@@ -0,0 +1,21 @@
+import pytest
+from employee import Employee
+from shift import Shift
+from schedule import Schedule
+
+def test_add_shift():
+    emp = Employee("Allen", "SL")
+    shift = Shift(emp,"2026-09-17", "9:00 AM", "5:00 PM")
+    schedule = Schedule() 
+    schedule.add_shift(shift)
+    assert shift in schedule.get_shifts()
+
+def test_add_shift_rejects_invalid_type():
+    with pytest.raises(TypeError):
+        shift = "Allen"
+        schedule = Schedule() 
+        schedule.add_shift(shift)
+
+def test_get_shift_empty():
+    schedule = Schedule() 
+    assert schedule.get_shifts() == []
