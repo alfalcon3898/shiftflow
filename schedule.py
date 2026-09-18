fa3c205734cd7b769a7c7d1dc0990af8d45e8db2
alfalcon3898 <maryann.falcon@snhu.edu>
2026-09-18 02:13:20 -0500

    Add Schedule class with shift storage and validation

---
+++ schedule.py
@@ -0,0 +1,16 @@
+from employee import Employee
+from shift import Shift
+from datetime import datetime, timedelta
+class Schedule:
+    def __init__(self) -> None:
+        self.__shifts = []
+    def add_shift(self, shift:Shift)->None:
+        if not isinstance(shift,Shift):
+            raise TypeError("Shift must be a Shift object.")
+        self.__shifts.append(shift)
+
+    def get_shifts(self)->list:
+        return self.__shifts.copy()
+    
+
+
